e763b75b421e32a683c1aaed4ed8fc3e5b5aaeca
Александр Ниценков <no_heaven227@inbox.ru>
2024-09-09 02:28:13 +0300

    homework1 is done

---
+++ service/src/test/java/com/nitsenkov/integration/ServiceIT.java
@@ -0,0 +1,12 @@
+package com.nitsenkov.integration;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ServiceIT {
+    @Test
+    public void shouldPassIT() {
+        assertTrue(true);
+    }
+}
