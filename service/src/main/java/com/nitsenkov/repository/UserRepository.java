1fb0dd915ca6ba376120226b6b481f3a18efcd59
Александр Ниценков <no_heaven227@inbox.ru>
2024-10-21 19:34:31 +0300

    homework7 is not finished

--- service/src/main/java/com/nitsenkov/repository/UserRepository.java
+++ service/src/main/java/com/nitsenkov/repository/UserRepository.java
@@ -2,11 +2,15 @@ package com.nitsenkov.repository;
 
 import com.nitsenkov.entity.User;
 import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public class UserRepository extends RepositoryBase<UUID, User> {
 
+    @Autowired
     public UserRepository(EntityManager entityManager) {
         super(User.class, entityManager);
     }
